f7863d7d9e0eb5f1af21c9c624207687e0854cbd
Nico Horrey <nico.horrey@web.de>
2022-10-24 15:41:06 +0200

    Implement graphs
    based on template method pattern

--- src/main/java/iopl/sheet1/Graph.java
+++ src/main/java/iopl/sheet1/Graph.java
@@ -1,6 +1,7 @@
 package iopl.sheet1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -27,6 +28,7 @@ class Graph {
 
     /**
      * Adds two vertices and an edge inbetween.
+     *
      * @param vertex1ID graph id of vertex 1
      * @param vertex2ID vertex graph id of vertex 2
      * @return The edge between vertex 1 and vertex 2
@@ -36,8 +38,15 @@ class Graph {
 
         Vertex v1 = this.vertices.stream().filter(v -> v.getId() == vertex1ID).findFirst().orElseThrow();
         Vertex v2 = this.vertices.stream().filter(v -> v.getId() == vertex2ID).findFirst().orElseThrow();
-        this.vertices.add(v1);
-        this.vertices.add(v2);
+
+        if(!contains(v1,this.vertices)){
+            this.vertices.add(v1);
+        }
+        if(!contains(v2,this.vertices)){
+            this.vertices.add(v2);
+        }
+
+
 
         Edge edge = this.createEdge(v1, v2);
         this.edges.add(edge);
@@ -57,6 +66,16 @@ class Graph {
         return new Vertex(this.vertexCount);
     }
 
+    boolean contains(Vertex v,List<Vertex> list){
+        for (Vertex inList: list
+             ) {
+            if(inList.getId()==v.getId()){
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     @Override
     public String toString() {
@@ -66,6 +85,65 @@ class Graph {
         this.edges.forEach(e -> sb.append(e).append("\n"));
 
         return sb.toString();
+    }
+
+    public void DFS(Vertex startVertex){
+        DFS(startVertex.getId());
+    }
+
+    public void DFS(int startId) {
+        boolean visited[] = new boolean[vertexCount]; //mark every vertex as unvisited
+
+
+        DFSrecursive(startId, visited);
+    }
+
+    public void DFS(LabeledVertex startLabeled){
+        DFS(startLabeled.getId());
+    }
+
+    private void DFSrecursive(int startId, boolean visited[]) {
+        visited[startId] = true; //mark startpoint
+
+
+        for (Vertex v: vertices
+             ) {
+            if(v.getId()==startId){
+                System.out.print(v+" ");
+
+            }
+
+
+        }
+       // System.out.print(startId + " ");
+
+
+
+        for (Edge e : edges
+        ) {
+            Vertex[] currentVertices = e.getVertices();
+            /*//System.out.println(e);
+            System.out.println(Arrays.toString(visited));
+            System.out.println("vertices" + this.vertices);
+
+            System.out.println(Arrays.toString(currentVertices));
+            System.out.println("starter" + currentVertices[0]);
+            System.out.println(startId);
+
+             */
+
+            if (currentVertices[0].getId() == startId) {
+                if (!visited[currentVertices[1].getId()]) {
+                    DFSrecursive(currentVertices[1].getId(), visited);
+                }
+            } else if (currentVertices[1].getId() == startId) {
+                if (!visited[currentVertices[0].getId()]) {
+                    DFSrecursive(currentVertices[0].getId(), visited);
+                }
+            }
+
+
+        }
     }
 }
 
@@ -93,4 +171,6 @@ class LabeledGraph<T> extends Graph {
 
         return new LabeledVertex<T>(this.vertexCount);
     }
+
+
 }
--- src/test/java/iopl/sheet1/GraphTests.java
+++ src/test/java/iopl/sheet1/GraphTests.java
@@ -2,6 +2,8 @@ package iopl.sheet1;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 // not really tests, just playing around a bit to see if something breaks...
 class GraphTests {
 
@@ -61,5 +63,103 @@ class GraphTests {
         edge.setWeight(10.10);
 
         System.out.println(wg);
+    }
+
+    @Test
+    public void testDFS() {
+
+        System.out.println("======DFS=====");
+
+        Graph g = new Graph();
+        g.addVertex();
+        g.addVertex();
+        g.addVertex();
+        g.addVertex();
+        g.addVertex();
+        g.addVertex();
+        g.addVertex();
+        g.addVertex(); //8
+
+        g.addEdge(4, 1);
+        g.addEdge(0, 2);
+        g.addEdge(3, 2);
+        g.addEdge(3, 5);
+        g.addEdge(5, 6);
+        g.addEdge(6, 4);
+        g.addEdge(2, 4);
+        g.addEdge(0, 5);
+        g.addEdge(5, 7);
+
+
+
+        System.out.print("DFS(0): ");
+        g.DFS(0);
+        System.out.println("");
+
+        System.out.print("DFS(4): ");
+        g.DFS(4);
+        System.out.println("");
+
+        System.out.print("DFS(7): ");
+        g.DFS(7);
+
+
+        System.out.println("");
+        System.out.println("");
+
+    }
+
+    @Test
+    public void testDFSlabel() {
+
+        System.out.println("======DFS - Label=====");
+
+        LabeledGraph<String> stringGraph = new LabeledGraph<>();
+
+        // not safe
+        LabeledVertex<String> v = (LabeledVertex<String>) stringGraph.addVertex();
+        v.setLabel("cat");
+        v = (LabeledVertex<String>) stringGraph.addVertex();
+        v.setLabel("test");
+        v = (LabeledVertex<String>) stringGraph.addVertex();
+        v.setLabel("tree");
+        v = (LabeledVertex<String>) stringGraph.addVertex();
+        v.setLabel("book");
+        v = (LabeledVertex<String>) stringGraph.addVertex();
+        v.setLabel("chest");
+        v = (LabeledVertex<String>) stringGraph.addVertex();
+        v.setLabel("bird");
+        v = (LabeledVertex<String>) stringGraph.addVertex();
+        v.setLabel("cow");
+        v = (LabeledVertex<String>) stringGraph.addVertex();
+        v.setLabel("pen");
+
+
+        stringGraph.addEdge(4, 1);
+        stringGraph.addEdge(0, 2);
+        stringGraph.addEdge(3, 2);
+        stringGraph.addEdge(3, 5);
+        stringGraph.addEdge(5, 6);
+        stringGraph.addEdge(6, 4);
+        stringGraph.addEdge(2, 4);
+        stringGraph.addEdge(0, 5);
+        stringGraph.addEdge(5, 7);
+
+
+        System.out.print("DFS- Label(0): ");
+        stringGraph.DFS(0);
+        System.out.println("");
+
+        System.out.print("DFS- Label(4): ");
+        stringGraph.DFS(4);
+        System.out.println("");
+
+        System.out.print("DFS- Label(7): ");
+        stringGraph.DFS(7);
+
+
+        System.out.println("");
+        System.out.println("");
+
     }
 }
